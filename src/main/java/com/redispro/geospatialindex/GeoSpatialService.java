2e2fee50d1083aefb16ed5caed0c5d0abbf5fc9f
gsadanala <ganesh8374387106@gmail.com>
2024-05-08 23:07:39 -0400

    Redis - GeoSpatial indexing

---
+++ src/main/java/com/redispro/geospatialindex/GeoSpatialService.java
@@ -0,0 +1,30 @@
+package com.redispro.geospatialindex;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.geo.Distance;
+import org.springframework.data.geo.GeoResults;
+import org.springframework.data.geo.Point;
+import org.springframework.data.redis.connection.RedisGeoCommands;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class GeospatialService {
+
+    private static final String GEO_KEY = "locations";
+
+    @Autowired
+    private RedisTemplate<String, String> redisTemplate;
+
+    public void addLocation(String locationName, double longitude, double latitude) {
+        redisTemplate.opsForGeo().add(GEO_KEY, new Point(longitude, latitude), locationName);
+    }
+
+    public GeoResults<RedisGeoCommands.GeoLocation<String>> findLocationsNearby(double longitude, double latitude, double distanceInKm) {
+        Point point = new Point(longitude, latitude);
+        Distance distance = new Distance(distanceInKm, RedisGeoCommands.DistanceUnit.KILOMETERS);
+        RedisGeoCommands.GeoRadiusCommandArgs args = RedisGeoCommands.GeoRadiusCommandArgs.newGeoRadiusArgs().includeDistance().includeCoordinates().sortAscending().limit(50);
+        return redisTemplate.opsForGeo().radius(GEO_KEY, point, distance, args);
+    }
+}
+
